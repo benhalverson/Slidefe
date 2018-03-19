fbc41957fe4f1c0ea2325fed2836d27c5fa46816
Ben Halverson <bhalverson@viscira.com>
2018-03-19 08:53:57 -0700

    sass is setup with CRA without ejecting

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Presenter from '../src/components/presenter/Presenter';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-         hello Viscira!
-        </p>
+          <Presenter slide={1}/>
       </div>
     );
   }
---
+++ src/components/presenter/Presenter.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class Presenter extends Component {
+    render() {
+        return (<div className="red">
+            hello Viscira! slide 1
+        </div>)
+    }
+}
+export default Presenter;
