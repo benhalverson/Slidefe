7294a8eb32c4ccdfdba55b088e94ec7c0fcdf253
Ben Halverson <bhalverson@viscira.com>
2018-03-20 23:01:24 -0700

    refactored code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route} from 'react-router-dom';
 // import Presenter from '../src/components/presenter/Presenter';
-import Slide1 from '../src/components/slide1/Slide1';
-import Slide2 from '../src/components/slide2/Slide2';
+// import Slide1 from './components/Slide1';
+// import Slide2 from './Slide2';
 import './App.css';
 import Canvas from "./components/Canvas";
 
